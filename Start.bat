3292760958cbfd959be33c4edb9992691ae3360e
RoboXploit <horror.championship@gmail.com>
2024-06-08 08:43:47 -0600

    Add files via upload

---
+++ Start.bat
@@ -0,0 +1,21 @@
+@echo off 
+
+color 2
+
+echo 88""Yb  dP"Yb  88""Yb  dP"Yb  Yb  dP 88""Yb 88      dP"Yb  88 888888 
+echo 88__dP dP   Yb 88__dP dP   Yb  YbdP  88__dP 88     dP   Yb 88   88   
+echo 88"Yb  Yb   dP 88""Yb Yb   dP  dPYb  88"""  88  .o Yb   dP 88   88   
+echo 88  Yb  YbodP  88oodP  YbodP  dP  Yb 88     88ood8  YbodP  88   88   
+echo -------------------------------------------------------------------
+echo Auto Farming
+echo Teleportation - TP to, TP coords, Teleport obj.
+echo Speed Hacks - Fly, No clip.
+echo Combat - Aimbot, God mode, Infinite Ammo.
+echo Utility - ESP, Infinite jump, Anti-AFK.
+echo Inventory - Item Spawner, Resource Multiplier
+echo ___
+echo ______
+echo ___
+set /p E=Pick Your Exploit Or Go To Scipt:
+
+pause
